4b2662a134874470c2baafd67902cb39640a0720
kenji_oshio <kenji_oshio@ot.olympus.co.jp>
2017-12-07 10:25:33 +0900

    This is initial project file.

---
+++ Summation/Summation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculation.Summation
+{
+    public class Summation
+    {
+        public float Add(float a, float b)
+        {
+            return a + b;
+        }
+
+    }
+}
---
+++ SummationTest/SummationTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TestClass = NUnit.Framework.TestFixtureAttribute;
+using TestMethod = NUnit.Framework.TestAttribute;
+using Calculation.Summation;
+
+namespace Calculation.Summation.Test
+{
+    [TestClass]
+    public class SummationTest
+    {
+        [TestMethod]
+        public void AddTwoNum()
+        {
+            // 1+2=3!!
+            Calculation.Summation.Summation c = new Calculation.Summation.Summation();
+            float r = c.Add(1.0f, 2.0f);
+            Assert.AreEqual(3.0f, r);
+        }
+        [TestMethod]
+        public void AddTwoNum2()
+        {
+            // 3+5=8!!
+            Calculation.Summation.Summation c = new Calculation.Summation.Summation();
+            float r = c.Add(3.0f, 5.0f);
+            Assert.AreEqual(8.0f, r);
+        }
+    }
+}
